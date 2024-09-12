8f8999eabaa38d4d6978414286c8e0c83df1ecbd
Victor Johansson <vijo07001@utb.vaxjo.se>
2024-09-12 13:21:02 +0200

    Add project files.

---
+++ upgift 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+namespace protject_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            {
+                Console.WriteLine("Your pc has crashed and requiers a restart");
+                Console.WriteLine("we are just colecting some crashreports from your pc");
+                Console.WriteLine("please wait while we are resolving your issue");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
